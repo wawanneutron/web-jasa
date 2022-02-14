efe6cd0b451ef72bb2f45149ebb46dfbcd3cdc5c
wawanneutron <wawanneutron1331@gmail.com>
2022-02-14 10:54:30 +0700

    layouts landing

--- resources/views/includes/landing/meta.blade.php
+++ resources/views/includes/landing/meta.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/favicon-16x16.png') }}">
 {{-- CSRF Token --}}
---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('includes.landing.meta')
+        <title>@yield('title') | {{ config('app.name') }}</title>
+        
+        {{-- style --}}
+        @stack('bifore-style')
+        @include('includes.landing.style')
+        @stack('after-style')
+    </head>
+
+    <body class="antialiased">
+        <div class="relative">
+            @include('includes.landing.header')
+            {{-- @include('sweetalert::alert') --}}
+            @yield('content')
+            @include('includes.landing.footer')
+            
+            {{-- script --}}
+            @stack('bifore-script')
+            @include('includes.landing.script')
+            @stack('after-script')
+            
+            {{-- modals --}}
+            @include('components.modal.login')
+            @include('components.modal.register')
+            @include('components.modal.register-success')
+        </div>
+    </body>
+</html>
