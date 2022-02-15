0a12fc9a2969700eedf3eaf2dad2e2b3aaf8c2d5
wawanneutron <wawanneutron1331@gmail.com>
2022-02-15 15:38:33 +0700

    update layouts landing

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
         <title>@yield('title') | {{ config('app.name') }}</title>
         
         {{-- style --}}
-        @stack('bifore-style')
+        @stack('before-style')
         @include('includes.landing.style')
         @stack('after-style')
     </head>
@@ -16,16 +16,16 @@
             {{-- @include('sweetalert::alert') --}}
             @yield('content')
             @include('includes.landing.footer')
-            
-            {{-- script --}}
-            @stack('bifore-script')
-            @include('includes.landing.script')
-            @stack('after-script')
-            
-            {{-- modals --}}
-            @include('components.modal.login')
-            @include('components.modal.register')
-            @include('components.modal.register-success')
         </div>
+        
+        {{-- modals --}}
+        @include('components.modal.login')
+        @include('components.modal.register')
+        @include('components.modal.register-success')
+
+        {{-- script --}}
+        @stack('bifore-script')
+        @include('includes.landing.script')
+        @stack('after-script')
     </body>
 </html>
