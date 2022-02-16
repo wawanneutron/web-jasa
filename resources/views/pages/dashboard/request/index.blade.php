f4885dd8c70a48d6e16802d106da5bc3354c679f
wawanneutron <wawanneutron1331@gmail.com>
2022-02-16 10:13:34 +0700

    empty data

--- resources/views/pages/dashboard/request/index.blade.php
+++ resources/views/pages/dashboard/request/index.blade.php
@@ -2,6 +2,24 @@
 
 @section('title', 'Requests')
 @section('content')
+    {{-- <div class="flex h-screen">
+        <div class="m-auto text-center">
+            <img src="{{ asset('/assets/images/empty-illustration.svg') }}" alt="" class="w-48 mx-auto">
+            <h2 class="mt-8 mb-1 text-2xl font-semibold text-gray-700">
+                There is No Request Yet
+            </h2>
+            <p class="text-sm text-gray-400">
+            It seems that you haven’t ordered any service. <br>
+            Let’s order your first service!
+            </p>
+
+            <div class="relative mt-0 md:mt-6">
+                <a href="{{ route('explore.landing') }}" target="__blank" class="px-4 py-2 mt-2 text-left text-white rounded-xl bg-serv-button">
+                    Find Services
+                </a>
+            </div>
+        </div>
+    </div> --}}
     <main class="h-full overflow-y-auto">
         <div class="container mx-auto">
             <div class="grid w-full gap-5 px-10 mx-auto md:grid-cols-12">
