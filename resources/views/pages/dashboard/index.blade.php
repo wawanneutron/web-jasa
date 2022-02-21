60dd3709563ae605c8276ba0e918348b15f20fd4
wawanneutron <wawanneutron1331@gmail.com>
2022-02-21 11:15:00 +0700

    dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -42,7 +42,7 @@
                                 <div>
                                     <img src="{{ asset('/assets/images/services-progress-icon.svg') }}" alt="" class="w-8 h-8">
                                 </div>
-                                <p class="mt-2 text-2xl font-semibold text-left text-gray-800">3</p>
+                                <p class="mt-2 text-2xl font-semibold text-left text-gray-800">{{ $progress }}</p>
                                 <p class="text-sm text-left text-gray-500">
                                     Services <br class="hidden lg:block">
                                     On Progress
@@ -54,7 +54,7 @@
                                 <div>
                                     <img src="{{ asset('/assets/images/services-completed-icon.svg') }}" alt="" class="w-8 h-8">
                                 </div>
-                                <p class="mt-2 text-2xl font-semibold text-left text-gray-800">144</p>
+                                <p class="mt-2 text-2xl font-semibold text-left text-gray-800">{{ $completed }}</p>
                                 <p class="text-sm text-left text-gray-500">
                                     Services <br class="hidden lg:block">
                                     Completed
@@ -66,7 +66,7 @@
                                 <div>
                                     <img src="{{ asset('/assets/images/new-freelancer-icon.svg') }}" alt="" class="w-8 h-8">
                                 </div>
-                                <p class="mt-2 text-2xl font-semibold text-left text-gray-800">3</p>
+                                <p class="mt-2 text-2xl font-semibold text-left text-gray-800">{{ $freelancer }}</p>
                                 <p class="text-sm text-left text-gray-500">
                                     New Freelancer <br class="hidden lg:block">
                                     Work for You
@@ -80,125 +80,84 @@
                                 Latest Orders
                             </h2>
                             <p class="text-sm text-gray-400">
-                                3 Total Orders On Progress
+                                {{ count($orders) }} Total Orders On Progress
                             </p>
                         </div>
-                        <table class="w-full mt-4" aria-label="Table">
-                            <thead>
-                                <tr class="text-sm font-normal text-left text-gray-900 border-b border-b-gray-600">
-                                    <th class="py-4" scope="">Name</th>
-                                    <th class="py-4" scope="">Services Name</th>
-                                    <th class="py-4" scope="">Deadline</th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white">
-                                <tr class="text-gray-700">
-                                    <td class="w-1/3 px-1 py-5">
-                                        <div class="flex items-center text-sm">
-                                            <div class="relative w-10 h-10 mr-3 rounded-full md:block">
-                                                <img class="object-cover w-full h-full rounded-full" src="https://randomuser.me/api/portraits/men/2.jpg" alt="" loading="lazy" />
-                                                <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                        @forelse ($orders as $order)
+                            <table class="w-full mt-4" aria-label="Table">
+                                <thead>
+                                    <tr class="text-sm font-normal text-left text-gray-900 border-b border-b-gray-600">
+                                        <th class="py-4" scope="">Name</th>
+                                        <th class="py-4" scope="">Services Name</th>
+                                        <th class="py-4" scope="">Deadline</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white">
+                                    <tr class="text-gray-700">
+                                        <td class="w-1/3 px-1 py-5">
+                                            <div class="flex items-center text-sm">
+                                                <div class="relative w-10 h-10 mr-3 rounded-full md:block">
+                                                    <img class="object-cover w-full h-full rounded-full" src="https://randomuser.me/api/portraits/men/2.jpg" alt="" loading="lazy" />
+                                                    <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div>
+                                                    <p class="font-medium text-black">{{ $order->user->name }}</p>
+                                                    @switch($order->order_status->id)
+                                                        @case(1)
+                                                            <p class="text-sm text-yellow-400">{{ $order->order_status->name }}</p>
+                                                        @break
+                                                        @case(2)
+                                                            <p class="text-sm text-yellow-400">{{ $order->order_status->name }}</p>
+                                                        @break
+                                                        @case(3)
+                                                            <p class="text-sm text-yellow-400">{{ $order->order_status->name }}</p>
+                                                        @break
+                                                        @case(4)
+                                                            <p class="text-sm text-yellow-400">{{ $order->order_status->name }}</p>
+                                                        @break
+                                                    @endswitch
+                                                </div>
                                             </div>
-                                            <div>
-                                                <p class="font-medium text-black">Siri Leaf</p>
-                                                <p class="text-sm text-yellow-400">On Progress</p>
+                                        </td>
+                                        <td class="w-2/4 px-1 py-5">
+                                            <div class="flex items-center text-sm">
+                                                <div class="relative w-10 h-10 mr-3 rounded-full md:block">
+                                                    @if ($order->service->thumbnail_Service[0] != null)
+                                                        <img class="object-cover w-full h-full rounded" src="{{ Storage::url($order->service->thumbnail_service[0]->thumbnail) }}" alt="" loading="lazy" />
+                                                    @else
+                                                        <img class="object-cover w-full h-full rounded" src="{{ ('/storage/assets/service/notfound.png') }}" alt="" loading="lazy" />
+                                                    @endif
+                                                    <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                </div>
+                                                <div>
+                                                    <p class="font-medium text-black">
+                                                        {{ $order->service->title }}
+                                                    </p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td class="w-2/4 px-1 py-5">
-                                        <div class="flex items-center text-sm">
-                                            <div class="relative w-10 h-10 mr-3 rounded-full md:block">
-                                                <img class="object-cover w-full h-full rounded" src="https://randomuser.me/api/portraits/men/3.jpg" alt="" loading="lazy" />
-                                                <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                            </div>
-                                            <div>
-                                                <p class="font-medium text-black">
-                                                    Design WordPress E-Commerce Modules
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="px-1 py-5 text-xs text-red-500">
-                                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline mb-1">
-                                            <path d="M7.0002 12.8332C10.2219 12.8332 12.8335 10.2215 12.8335 6.99984C12.8335 3.77818 10.2219 1.1665 7.0002 1.1665C3.77854 1.1665 1.16687 3.77818 1.16687 6.99984C1.16687 10.2215 3.77854 12.8332 7.0002 12.8332Z" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
-                                            <path d="M7 3.5V7L9.33333 8.16667" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-
-                                        1 May 2021
-                                    </td>
-                                </tr>
-                                <tr class="text-gray-700">
-                                    <td class="w-1/3 px-1 py-5">
-                                        <div class="flex items-center text-sm">
-                                            <div class="relative w-10 h-10 mr-3 rounded-full md:block">
-                                                <img class="object-cover w-full h-full rounded-full" src="https://randomuser.me/api/portraits/men/4.jpg" alt="" loading="lazy" />
-                                                <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                            </div>
-                                            <div>
-                                                <p class="font-medium text-black">Miles John</p>
-                                                <p class="text-sm text-yellow-400">On Progress</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="w-2/4 px-1 py-5">
-                                        <div class="flex items-center text-sm">
-                                            <div class="relative w-10 h-10 mr-3 rounded-full md:block">
-                                                <img class="object-cover w-full h-full rounded" src="https://randomuser.me/api/portraits/men/5.jpg" alt="" loading="lazy" />
-                                                <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                            </div>
-                                            <div>
-                                                <p class="font-medium text-black">
-                                                    Fix Any Issue on Your WordPress Website
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="px-1 py-5 text-xs text-red-500">
-                                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline mb-1">
-                                            <path d="M7.0002 12.8332C10.2219 12.8332 12.8335 10.2215 12.8335 6.99984C12.8335 3.77818 10.2219 1.1665 7.0002 1.1665C3.77854 1.1665 1.16687 3.77818 1.16687 6.99984C1.16687 10.2215 3.77854 12.8332 7.0002 12.8332Z" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
-                                            <path d="M7 3.5V7L9.33333 8.16667" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-
-                                        1 May 2021
-                                    </td>
-                                </tr>
-                                <tr class="text-gray-700">
-                                    <td class="w-1/3 px-1 py-5">
-                                        <div class="flex items-center text-sm">
-                                            <div class="relative w-10 h-10 mr-3 rounded-full md:block">
-                                                <img class="object-cover w-full h-full rounded-full" src="https://randomuser.me/api/portraits/men/6.jpg" alt="" loading="lazy" />
-                                                <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                            </div>
-                                            <div>
-                                                <p class="font-medium text-black">Alexa Sara</p>
-                                                <p class="text-sm text-yellow-400">On Progress</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="w-2/4 px-1 py-5">
-                                        <div class="flex items-center text-sm">
-                                            <div class="relative w-10 h-10 mr-3 rounded-full md:block">
-                                                <img class="object-cover w-full h-full rounded" src="https://randomuser.me/api/portraits/men/7.jpg" alt="" loading="lazy" />
-                                                <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                            </div>
-                                            <div>
-                                                <p class="font-medium text-black">
-                                                    Design WordPress E-Commerce Modules
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="px-1 py-5 text-xs text-red-500">
-                                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline mb-1">
-                                            <path d="M7.0002 12.8332C10.2219 12.8332 12.8335 10.2215 12.8335 6.99984C12.8335 3.77818 10.2219 1.1665 7.0002 1.1665C3.77854 1.1665 1.16687 3.77818 1.16687 6.99984C1.16687 10.2215 3.77854 12.8332 7.0002 12.8332Z" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
-                                            <path d="M7 3.5V7L9.33333 8.16667" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-
-                                        1 May 2021
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                                        </td>
+                                        <td class="px-1 py-5 text-xs text-red-500">
+                                            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline mb-1">
+                                                <path d="M7.0002 12.8332C10.2219 12.8332 12.8335 10.2215 12.8335 6.99984C12.8335 3.77818 10.2219 1.1665 7.0002 1.1665C3.77854 1.1665 1.16687 3.77818 1.16687 6.99984C1.16687 10.2215 3.77854 12.8332 7.0002 12.8332Z" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M7 3.5V7L9.33333 8.16667" stroke="#F26E6E" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                            
+                                            {{ \Carbon\Carbon::create($order->expired)->format('F d, Y') }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        @empty
+                            <div class="m-auto mt-5 text-center">
+                                <img src="{{ asset('/assets/images/empty-illustration.svg') }}" alt="" class="w-15 mx-auto">
+                                <h2 class="mt-8 mb-1 text-2sm font-semibold text-gray-700">
+                                    There is No Order Yet
+                                </h2>
+                                <p class="text-xs text-gray-400">
+                                Looks like no orders have come in yet
+                                </p>
+                            </div>
+                        @endforelse
                     </div>
                 </main>
                 <aside class="p-4 lg:col-span-5 md:col-span-12 md:pt-0">
